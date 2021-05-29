8a5538b5ed1906e00e7badbb13a695666c96461c
ShivaKeerthi0 <60295265+ShivaKeerthi0@users.noreply.github.com>
2021-05-29 15:32:15 +0530

    Updated

--- Go/hi.go
+++ Go/hi.go
@@ -13,43 +13,49 @@ import (
 )
 
 func insHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("invoked!")
-
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS")
+	fmt.Fprint(w, "invoked")
 
-	var mail string = r.FormValue("cmail")
-	var cid string = r.FormValue("cid")
-	var cimg string = r.FormValue("cimg")
-	var cname string = r.FormValue("cname")
-	var cprice string = r.FormValue("cprice")
-	db, err := sql.Open("mysql", "root@tcp(localhost)/tutor")
+	if r.Method == "POST" {
 
-	if err != nil {
-		fmt.Println(err.Error())
+		var mail string = r.FormValue("cmail")
+
+		var cid string = r.FormValue("cid")
+		var cimg string = r.FormValue("cimg")
+		var cname string = r.FormValue("cname")
+		var cprice string = r.FormValue("cprice")
+
+		db, err := sql.Open("mysql", "root@tcp(localhost)/tutor")
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		defer db.Close()
+		statement, err := db.Prepare("INSERT INTO coursesreg SET email=?, cid=?, cimg=?,cname=?,cprice=?")
+		if err != nil {
+			json.NewEncoder(w).Encode("connection failed!")
+			return
+		}
+		defer statement.Close()
+		insert, err := statement.Exec(mail, cid, cimg, cname, cprice)
+		if err != nil {
+			json.NewEncoder(w).Encode("Insertion Failed")
+			return
+		}
+		fmt.Println(insert.RowsAffected())
+		json.NewEncoder(w).Encode("successfull")
 	}
-	defer db.Close()
-	statement, err := db.Prepare("INSERT INTO coursesreg SET email=?, cid=?, cimg=?,cname=?,cprice=?")
-	if err != nil {
-		json.NewEncoder(w).Encode("connection failed!")
-		return
-	}
-	defer statement.Close()
-	insert, err := statement.Exec(mail, cid, cimg, cname, cprice)
-	if err != nil {
-		json.NewEncoder(w).Encode("Insertion Failed")
-		return
-	}
-	fmt.Println(insert.RowsAffected())
-	json.NewEncoder(w).Encode("successfull")
 }
 func main() {
 	fmt.Println("Hi!")
 
 	myRouter := mux.NewRouter()
 
-	myRouter.HandleFunc("/coursesreg", insHandler).Methods("POST")
+	myRouter.HandleFunc("/coursesreg", insHandler)
 	log.Fatal(http.ListenAndServe(":8082", myRouter))
 
 }
